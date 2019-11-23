d49db651f156bb6ae4537a388c818502675d76d5
itcrazy2016 <itcrazy2016@163.com>
2019-11-23 22:06:24 +0800

    last commit

--- src/main/java/com/gupao/springbootdemo/controller/K8SController.java
+++ src/main/java/com/gupao/springbootdemo/controller/K8SController.java
@@ -11,6 +11,6 @@ public class K8SController {
 
     @RequestMapping("/k8s")
     public String k8s(){
-        return "hello K8s <br/>11122211111 22  11.23 Jack";
+        return "hello K8s Jack";
     }
 }
